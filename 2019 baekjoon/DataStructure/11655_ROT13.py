9a8e83815f1cf8c87d2386da7aec7dba7dab6610
choiyeonseok <smr603@snu.ac.kr>
2019-08-24 23:35:23 +0900

    자료구조1 - ROT13

---
+++ 2019 baekjoon/DataStructure/11655_ROT13.py
@@ -0,0 +1,21 @@
+def ROT(sentence):
+    for i in range(len(sentence)):
+        pw = ord(sentence[i]) + 13
+        #대문자라면,
+        if (ord(sentence[i]) >= 65) and (ord(sentence[i]) <= 90):
+            if pw > 90:
+                sentence[i] = chr(pw - 26)
+            else:
+                sentence[i] = chr(pw)
+        #소문자라면,
+        elif (ord(sentence[i]) >= 97) and (ord(sentence[i]) <= 122):
+            if pw > 122:
+                sentence[i] = chr(pw - 26)
+            else:
+                sentence[i] = chr(pw)
+    return ''.join(sentence)
+
+
+sentence = list(input())
+print(ROT(sentence))
+
