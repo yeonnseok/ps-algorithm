72a95e7c06df82eeb9716db3f7efa59ee2323499
choiyeonseok <smr603@snu.ac.kr>
2019-08-25 03:19:47 +0900

    수학 - 소수구하기

---
+++ 2019 baekjoon/Math/1929_findPrime.py
@@ -0,0 +1,23 @@
+# M이상 N 이하의 소수를 모두 출력하는 프로그램을 작성하시오.
+
+
+def findPrime(src):
+    m, n = src[0], src[1]
+    ans = []
+    for num in range(m, n + 1):
+        if num > 1:
+            t_count = 0
+            for j in range(2, num):
+                if num % j == 0:
+                    t_count += 1
+            if t_count == 0:
+                ans.append(num)
+    return ans
+
+
+def main():
+    src = list(map(int, input().split()))
+    list(map(print, findPrime(src)))
+
+
+main()
