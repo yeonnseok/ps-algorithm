c0e6006d58762aa6a7f9d5ba062e432aaef79a37
choiyeonseok <smr603@snu.ac.kr>
2019-08-30 04:13:53 +0900

    DP - 가장긴 증가하는 수열4

---
+++ 2019 baekjoon/DynamicProgramming/14002_the_longest_increase4.py
@@ -0,0 +1,32 @@
+def solve(n, src):
+    d = [0] * (n + 1)
+
+    for i in range(1, n + 1):
+        d[i] = 1
+        for j in range(i):
+            if src[j] < src[i] and d[j] + 1 > d[i]:
+                d[i] = d[j] + 1
+
+    ans = []
+    n = 4
+    for i in reversed(range(1, len(src))): # 6, 5, 4, 3, 2, 1
+        for j in reversed(range(1, max(d) + 1)):
+            if d[i] == j and j == n:
+                ans.insert(0, i)
+                n -= 1
+
+    array = []
+    for i in ans:
+        array.append(src[i])
+
+    print(max(d))
+    print(array)
+
+
+def main():
+    n = int(input())
+    src = [0] + list(map(int, input().split()))
+    solve(n, src)
+
+
+main()
