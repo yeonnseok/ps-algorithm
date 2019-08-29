619e64334746d60f4f26e1751b80a6049a29664e
choiyeonseok <smr603@snu.ac.kr>
2019-08-29 19:02:11 +0900

    DP - 1,2,3 더하기

---
+++ 2019 baekjoon/DynamicProgramming/9095_1,2,3adding.py
@@ -0,0 +1,21 @@
+def solve(n):
+    d = [0] * (n + 1)
+    if n <= 1:
+        return 1
+    if n == 2:
+        return 2
+    if d[n]:
+        return d[n]
+
+    d[n] = solve(n-1) + solve(n-2) + solve(n-3)
+    return d[n]
+
+
+def main():
+    numOfCase = int(input())
+    for i in range(numOfCase):
+        n = int(input())
+        print(solve(n))
+
+
+main()
