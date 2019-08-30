94d44bb68b417e4bb47023431cf8cb77bb20df7a
choiyeonseok <smr603@snu.ac.kr>
2019-08-30 20:52:58 +0900

    DP - 연속합2

---
+++ 2019 baekjoon/DynamicProgramming/13398_sequentialAdd2.py
@@ -0,0 +1,23 @@
+def solve(n, src):
+    dl = [0] * (n + 1)
+    dr = [0] * (n + 1)
+
+    for i in range(1, n + 1):
+        dl[i] = max(dl[i-1] + src[i], src[i])
+    for i in reversed(range(n)):
+        dr[i] = max(dr[i+1] + src[i], src[i])
+
+    ans = max(dl)
+    for i in range(1, n):
+        if ans < dl[i - 1] + dr[i + 1]:
+            ans = dl[i - 1] + dr[i + 1]
+
+    return ans
+
+def main():
+    n = int(input())
+    src = [0] + list(map(int, input().split()))
+    print(solve(n, src))
+
+
+main()
