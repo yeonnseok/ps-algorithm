f129a7a0f3b3cdefa197b5b2bcd1d2ab6d27b9e4
choiyeonseok <smr603@snu.ac.kr>
2019-08-30 19:57:48 +0900

    가장 큰 증가하는 수열

---
+++ 2019 baekjoon/DynamicProgramming/11055_the_biggest_increase.py
@@ -0,0 +1,21 @@
+def solve(n, src):
+    d = [0] * (n + 1)
+
+    for i in range(1, n + 1):
+        d[i] = src[i]
+        for j in range(i):
+            if src[i] > src[j] and d[j] + src[i] > d[i]:
+                d[i] = d[j] + src[i]
+
+    return max(d)
+
+
+
+
+def main():
+    n = int(input())
+    src = [0] + list(map(int, input().split()))
+    print(solve(n, src))
+
+
+main()
