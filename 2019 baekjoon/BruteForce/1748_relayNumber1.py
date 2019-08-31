346fc61fcba360e219631eef7cbce879d98dd16e
choiyeonseok <smr603@snu.ac.kr>
2019-08-31 14:10:55 +0900

    BF - 수이어 쓰기 1

---
+++ 2019 baekjoon/BruteForce/1748_relayNumber1.py
@@ -0,0 +1,31 @@
+def solve(n):
+    count = 0
+    for i in range(1, n + 1):
+        if i < 10:
+            count += 1
+        elif i < 100:
+            count += 2
+        elif i < 1000:
+            count += 3
+        elif i < 10000:
+            count += 4
+        elif i < 100000:
+            count += 5
+        elif i < 1000000:
+            count += 6
+        elif i < 10000000:
+            count += 7
+        elif i < 100000000:
+            count += 8
+        else:
+            count += 9
+
+    return count
+
+
+def main():
+    n = int(input())
+    print(solve(n))
+
+
+main()
