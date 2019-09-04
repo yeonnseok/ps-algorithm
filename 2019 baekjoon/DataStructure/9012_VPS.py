675736caec50bb0bd87afc37f23bbc92c7bee746
choiyeonseok <smr603@snu.ac.kr>
2019-09-04 21:13:27 +0900

    자료구조1 - 괄호(renew)

--- 2019 baekjoon/DataStructure/9012_VPS.py
+++ 2019 baekjoon/DataStructure/9012_VPS.py
@@ -1,24 +1,47 @@
-def validate(ps):
-    open, close = 0, 0
-    for i in range(len(ps)):
-        if ps[i] == "(":
-            open += 1
-        elif ps[i] == ")":
-            close += 1
-    if open == close:
-        return "YES"
-    else:
+# def validate(ps):
+#     open, close = 0, 0
+#     for i in range(len(ps)):
+#         if ps[i] == "(":
+#             open += 1
+#         elif ps[i] == ")":
+#             close += 1
+#     if open == close:
+#         return "YES"
+#     else:
+#         return "NO"
+#
+#
+# numOfCase = int(input())
+# ans = []
+# for i in range(numOfCase):
+#     ps = list(input())
+#     ans.append(validate(ps))
+#
+# list(map(print, ans))
+
+
+def solve(src):
+    stack = []
+    for i in range(len(src)):
+        if src[i] == "(":
+            stack.append(src[i])
+        else:
+            try:
+                stack.pop()
+            except:
+                return "NO"
+    if stack:
         return "NO"
+    else:
+        return "YES"
 
 
-numOfCase = int(input())
-ans = []
-for i in range(numOfCase):
-    # characters별로 분절하여 list에 담고싶으면 그냥 list(string)하면 된다.
-    ps = list(input())
-    ans.append(validate(ps))
-
-list(map(print, ans))
+def main():
+    numOfCase = int(input())
+    for _ in range(numOfCase):
+        src = list(input())
+        print(solve(src))
 
 
+main()
 
