71b8b8c1e2e7db012be3ecec76297e3a68ecd975
choiyeonseok <smr603@snu.ac.kr>
2019-09-04 20:46:09 +0900

    자료구조1 - 단어뒤집기(renew)

---
+++ 2019 baekjoon/BruteForce/9095_1,2,3addBF.py
@@ -0,0 +1,14 @@
+def solve(n):
+    count = 0
+    temp = []
+    pass
+
+
+def main():
+    numOfCase = int(input())
+    for _ in range(numOfCase):
+        n = int(input())
+        print(solve(n))
+
+
+main()
--- 2019 baekjoon/DataStructure/9093_wordConvert.py
+++ 2019 baekjoon/DataStructure/9093_wordConvert.py
@@ -1,23 +1,46 @@
-def convert(word):
-    word = list(word)
-    for i in range(len(word)//2):
-        temp = word[i]
-        word[i] = word[-1-i]
-        word[-1-i] = temp
-    word = ''.join(word)
-    return word
+# def convert(word):
+#     word = list(word)
+#     for i in range(len(word)//2):
+#         temp = word[i]
+#         word[i] = word[-1-i]
+#         word[-1-i] = temp
+#     word = ''.join(word)
+#     return word
+#
+#
+# numOfSent = int(input())
+# ans = []
+# for i in range(numOfSent):
+#     bucket = input().split()
+#     for j in range(len(bucket)):
+#         bucket[j] = convert(bucket[j])
+#     sentence = ' '.join(bucket)
+#     ans.append(sentence)
+#
+# # 한줄로 리스트 내부 요소 하나씩 출력하기
+# list(map(print, ans))
 
 
-numOfSent = int(input())
-ans = []
-for i in range(numOfSent):
-    bucket = input().split()
-    for j in range(len(bucket)):
-        bucket[j] = convert(bucket[j])
-    sentence = ' '.join(bucket)
-    ans.append(sentence)
+def solve(src):
+    stack = []
+    ans = []
+    for i in range(len(src)):
+        if src[i] == " " or src[i] == "\n":
+            while stack:
+                ans.append(stack.pop())
+            ans.append(src[i])
+        else:
+            stack.append(src[i])
+    while stack:
+        ans.append(stack.pop())
+    return ans
 
-# 한줄로 리스트 내부 요소 하나씩 출력하기
-list(map(print, ans))
+
+def main():
+    numOfCase = int(input())
+    for _ in range(numOfCase):
+        src = list(input())
+        print(solve(src))
 
 
+main()
