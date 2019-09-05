33ec1d330eb31d2a144dc57ce062872051024214
choiyeonseok <smr603@snu.ac.kr>
2019-09-05 12:48:20 +0900

    자료구조1 - 쇠막대기(renew)

--- 2019 baekjoon/DataStructure/10799_steelRod.py
+++ 2019 baekjoon/DataStructure/10799_steelRod.py
@@ -1,45 +1,69 @@
-from functools import reduce
+# from functools import reduce
+#
+#
+# def findLasers(li):
+#     lasers = []
+#     for i in range(len(li)):
+#         if li[i] == "(" and li[i+1] == ")":
+#             lasers.append((i, i+1))
+#     return lasers
+#
+#
+# def add(a, b):
+#     return a + b
+#
+#
+# def findRods(li):
+#     stack = []
+#     rods = []
+#     lasers = list(reduce(lambda x, y: add(x, y), findLasers(li)))
+#     for i in range(len(li)):
+#         if i in lasers:
+#             continue
+#         else:
+#             if li[i] == "(":
+#                 stack.append(i)
+#             elif li[i] == ")":
+#                 rods.append((stack.pop(), i))
+#     return rods
+#
+#
+# def main():
+#     src = input()
+#     lasers = findLasers(src)
+#     rods = findRods(src)
+#     count = 0
+#     for i in range(len(rods)):
+#         for j in range(len(lasers)):
+#             if lasers[j][0] in list(range(rods[i][0], rods[i][1])):
+#                 count += 1
+#         count += 1
+#     print(count)
+#
+#
+# main()
+#
 
 
-def findLasers(li):
-    lasers = []
-    for i in range(len(li)):
-        if li[i] == "(" and li[i+1] == ")":
-            lasers.append((i, i+1))
-    return lasers
-
-
-def add(a, b):
-    return a + b
-
-
-def findRods(li):
+def solve(src):
     stack = []
-    rods = []
-    lasers = list(reduce(lambda x, y: add(x, y), findLasers(li)))
-    for i in range(len(li)):
-        if i in lasers:
-            continue
-        else:
-            if li[i] == "(":
-                stack.append(i)
-            elif li[i] == ")":
-                rods.append((stack.pop(), i))
-    return rods
+    count = 0
+    for i in range(len(src)):
+        if src[i] == "(":
+            stack.append(i)
+        elif src[i] == ")":
+            if i - stack[-1] == 1:
+                stack.pop()
+                count += len(stack)
+            else:
+                stack.pop()
+                count += 1
+    return count
 
 
 def main():
-    src = input()
-    lasers = findLasers(src)
-    rods = findRods(src)
-    count = 0
-    for i in range(len(rods)):
-        for j in range(len(lasers)):
-            if lasers[j][0] in list(range(rods[i][0], rods[i][1])):
-                count += 1
-        count += 1
-    print(count)
+    src = list(input())
+    print(solve(src))
 
 
 main()
-
