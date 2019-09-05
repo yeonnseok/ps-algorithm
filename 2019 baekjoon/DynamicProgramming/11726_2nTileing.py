64ce26a1fb00003c3dadb70588c6dbda3bb5f3ea
choiyeonseok <smr603@snu.ac.kr>
2019-09-06 01:23:45 +0900

    DP - 2n타일링 Bottom-up (renew)

--- 2019 baekjoon/DynamicProgramming/11726_2nTileing.py
+++ 2019 baekjoon/DynamicProgramming/11726_2nTileing.py
@@ -1,22 +1,38 @@
 # D(N) = D(N-1) + D(N-2)
 
 
-def tileing(n):
+# def tileing(n):
+#     d = [0] * (n + 1)
+#     if n <= 1:
+#         return 1
+#     if d[n]:
+#         return d[n]
+#     d[n] = tileing(n-1) + tileing(n-2)
+#     return d[n]
+#
+#
+# def main():
+#     num = int(input())
+#     print(tileing(num))
+#
+#
+# main()
+
+
+def solve(n):
     d = [0] * (n + 1)
-    if n <= 1:
-        return 1
-    if d[n]:
-        return d[n]
-    d[n] = tileing(n-1) + tileing(n-2)
+    d[0] = 1
+    d[1] = 1
+    for i in range(2, n + 1):
+        d[i] = d[i - 1] + d[i - 2]
     return d[n]
 
 
 def main():
-    num = int(input())
-    print(tileing(num))
+    n = int(input())
+    print(solve(n))
 
 
 main()
-
 
 
