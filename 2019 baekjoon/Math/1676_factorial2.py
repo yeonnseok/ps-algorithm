8cc6226c6478d162a1a7461a29845e9280feb49a
choiyeonseok <smr603@snu.ac.kr>
2019-09-05 21:34:38 +0900

    수학1 - 팩토리얼 0의 갯수(renew)

--- 2019 baekjoon/Math/1676_factorial2.py
+++ 2019 baekjoon/Math/1676_factorial2.py
@@ -1,22 +1,35 @@
-def factorial(num):
-    if num == 1:
-        return 1
-    return num * factorial(num - 1)
-
-
-def factorial_count(num):
-    target = list(str(factorial(num)))
-    count = 0
-    for i in reversed(range(len(target))):
-        if target[i] == '0':
-            count += 1
-        else:
-            return count
+# def factorial(num):
+#     if num == 1:
+#         return 1
+#     return num * factorial(num - 1)
+#
+#
+# def factorial_count(num):
+#     target = list(str(factorial(num)))
+#     count = 0
+#     for i in reversed(range(len(target))):
+#         if target[i] == '0':
+#             count += 1
+#         else:
+#             return count
+#
+#
+# def main():
+#     num = int(input())
+#     print(factorial_count(num))
+#
+#
+# main()
 
 
 def main():
     num = int(input())
-    print(factorial_count(num))
+    i = 5
+    ans = 0
+    while i <= num:
+        ans += int(num/i)
+        i *= 5
+    print(ans)
 
 
 main()
