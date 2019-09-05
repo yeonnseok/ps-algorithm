326c67784a5484f241111a0e12a1c49cfe99908b
choiyeonseok <smr603@snu.ac.kr>
2019-09-05 11:40:29 +0900

    자료구조1 - 단어뒤집기2(renew)

--- 2019 baekjoon/DataStructure/1158_josepus.py
+++ 2019 baekjoon/DataStructure/1158_josepus.py
@@ -1,25 +1,49 @@
-def josepus(n, k):
-    q = []
+# def josepus(n, k):
+#     q = []
+#     ans = []
+#     idx = 0
+#     for i in range(1, n+1):
+#         q.append(i)
+#     while len(q) > 0:
+#         idx += k - 1
+#         ans.append(q.pop(idx))
+#         if idx == len(q):
+#             idx = 0
+#         if idx + 2 >= len(q):
+#             idx = -(idx + 2 - len(q))
+#         if len(q) == k-1:
+#             ans.append(q.pop(idx))
+#         if len(q) == 1:
+#             ans.append(q.pop())
+#             break
+#     return ans
+#
+#
+# #애매하지만 일단 성공
+# src = list(map(int, input().split()))
+# print(josepus(src[0], src[1]))
+#
+
+import queue
+
+
+def solve(src):
+    n, k = src[0], src[1]
+    q = queue.Queue()
     ans = []
-    idx = 0
-    for i in range(1, n+1):
-        q.append(i)
-    while len(q) > 0:
-        idx += k - 1
-        ans.append(q.pop(idx))
-        if idx == len(q):
-            idx = 0
-        if idx + 2 >= len(q):
-            idx = -(idx + 2 - len(q))
-        if len(q) == k-1:
-            ans.append(q.pop(idx))
-        if len(q) == 1:
-            ans.append(q.pop())
-            break
+    for i in range(1, n + 1):
+        q.put(i)
+
+    while q.qsize() != 0:
+        for i in range(k-1):
+            q.put(q.get())
+        ans.append(q.get())
     return ans
 
 
-#애매하지만 일단 성공
-src = list(map(int, input().split()))
-print(josepus(src[0], src[1]))
+def main():
+    src = list(map(int, input().split()))
+    print(solve(src))
 
+
+main()
--- 2019 baekjoon/DataStructure/17413_wordConvert2.py
+++ 2019 baekjoon/DataStructure/17413_wordConvert2.py
@@ -1,46 +1,79 @@
-def convert(word):
-    word = list(word)
-    for i in range(len(word)//2):
-        temp = word[i]
-        word[i] = word[-1-i]
-        word[-1-i] = temp
-    return ''.join(word)
+# def convert(word):
+#     word = list(word)
+#     for i in range(len(word)//2):
+#         temp = word[i]
+#         word[i] = word[-1-i]
+#         word[-1-i] = temp
+#     return ''.join(word)
+#
+#
+# def pure_case(tg_sentence):
+#     tg_sentence = tg_sentence.split()
+#     for i in range(len(tg_sentence)):
+#         tg_sentence[i] = convert(tg_sentence[i])
+#     return ' '.join(tg_sentence)
+#
+#
+# def diff_case(tg_sentence):
+#     open = []
+#     close = []
+#     ans = []
+#     for i in range(len(tg_sentence)):
+#         if tg_sentence[i] == '<':
+#             open.append(i)
+#         elif tg_sentence[i] == '>':
+#             close.append(i)
+#     op = len(open)
+#     ans.append(pure_case(tg_sentence[:open[0]]))
+#     for i in range(op): #0, 1
+#         ans.append(tg_sentence[open[i]:close[i] + 1])
+#         if i + 1 > op - 1:
+#             ans.append(pure_case(tg_sentence[close[i] + 1:]))
+#         else:
+#             ans.append(pure_case(tg_sentence[close[i] + 1:open[i+1]]))
+#     return ''.join(ans)
+#
+#
+# tg_sentence = input()
+# if '<' not in tg_sentence and '>' not in tg_sentence:
+#     print(pure_case(tg_sentence))
+# else:
+#     print(diff_case(tg_sentence))
+#
+#
+# #뿌듯..
+#
+#
 
 
-def pure_case(tg_sentence):
-    tg_sentence = tg_sentence.split()
-    for i in range(len(tg_sentence)):
-        tg_sentence[i] = convert(tg_sentence[i])
-    return ' '.join(tg_sentence)
-
-
-def diff_case(tg_sentence):
-    open = []
-    close = []
+def solve(src):
+    tag = False
+    stack = []
     ans = []
-    for i in range(len(tg_sentence)):
-        if tg_sentence[i] == '<':
-            open.append(i)
-        elif tg_sentence[i] == '>':
-            close.append(i)
-    op = len(open)
-    ans.append(pure_case(tg_sentence[:open[0]]))
-    for i in range(op): #0, 1
-        ans.append(tg_sentence[open[i]:close[i] + 1])
-        if i + 1 > op - 1:
-            ans.append(pure_case(tg_sentence[close[i] + 1:]))
-        else:
-            ans.append(pure_case(tg_sentence[close[i] + 1:open[i+1]]))
+    for i in range(len(src)):
+        if src[i] == "<":
+            tag = True
+            while stack:
+                ans.append(stack.pop())
+            ans.append(src[i])
+        elif src[i] == ">":
+            tag = False
+            ans.append(src[i])
+        elif tag:
+            ans.append(src[i])
+        elif tag is False:  # tag is false
+            stack.append(src[i])
+        elif src[i] == " ":
+            while stack:
+                ans.append(stack.pop())
+            ans.append(src[i])
+
     return ''.join(ans)
 
 
-tg_sentence = input()
-if '<' not in tg_sentence and '>' not in tg_sentence:
-    print(pure_case(tg_sentence))
-else:
-    print(diff_case(tg_sentence))
+def main():
+    src = input()
+    print(solve(src))
 
 
-#뿌듯..
-
-
+main()
