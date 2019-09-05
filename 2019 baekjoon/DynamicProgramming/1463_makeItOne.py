a2abcbfd421981bdce31e71b220f01a87090f072
choiyeonseok <smr603@snu.ac.kr>
2019-09-06 01:10:40 +0900

    DP - 1로만들기bottom-up (renew)

--- 2019 baekjoon/DynamicProgramming/1463_makeItOne.py
+++ 2019 baekjoon/DynamicProgramming/1463_makeItOne.py
@@ -1,28 +1,48 @@
-def makeOne(n):
-    d = [0] * (n+1)
-    if n == 1:
-        return 0
-    if d[n]:
-        return d[n]
+# def makeOne(n):
+#     d = [0] * (n+1)
+#     if n == 1:
+#         return 0
+#     if d[n]:
+#         return d[n]
+#
+#     d[n] = makeOne(n - 1) + 1
+#
+#     if n % 3 == 0:
+#         temp = makeOne(n//3) + 1
+#         if d[n] > temp:
+#             d[n] = temp
+#
+#     if n % 2 == 0:
+#         temp = makeOne(n//2) + 1
+#         if d[n] > temp:
+#             d[n] = temp
+#
+#     return d[n]
+#
+#
+# def main():
+#     num = int(input())
+#     print(makeOne(num))
+#
+#
+# main()
 
-    d[n] = makeOne(n - 1) + 1
 
-    if n % 3 == 0:
-        temp = makeOne(n//3) + 1
-        if d[n] > temp:
-            d[n] = temp
-
-    if n % 2 == 0:
-        temp = makeOne(n//2) + 1
-        if d[n] > temp:
-            d[n] = temp
-
+def solve(n):
+    d = [0] * (n + 1)
+    d[1] = 0
+    for i in range(2, n + 1):
+        d[i] = d[i-1] + 1
+        if (i % 2 == 0) and (d[i] > d[i // 2] + 1):
+            d[i] = d[i // 2] + 1
+        if (i % 3 == 0) and (d[i] > d[i // 3] + 1):
+            d[i] = d[i // 3] + 1
     return d[n]
 
 
 def main():
     num = int(input())
-    print(makeOne(num))
+    print(solve(num))
 
 
 main()
