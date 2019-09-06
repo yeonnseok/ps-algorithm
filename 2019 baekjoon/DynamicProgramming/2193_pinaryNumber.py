cdb9c23e3c34d50d47d38727e4fb285dd4067eb8
choiyeonseok <smr603@snu.ac.kr>
2019-09-06 14:25:55 +0900

    DP - 이친수 Bottom-up (renew)

--- 2019 baekjoon/DynamicProgramming/2193_pinaryNumber.py
+++ 2019 baekjoon/DynamicProgramming/2193_pinaryNumber.py
@@ -1,21 +1,31 @@
+# def solve(n):
+#     d = []
+#     for i in range(n+1):
+#         d.append([0] * 2)
+#     # d[i][0] = d[i-1][0] + d[i-1][1]
+#     # d[i][1] = d[i-1][0]
+#     # 초기값 설정
+#     d[1][0] = 0
+#     d[1][1] = 1
+#
+#     for i in range(2, n+1):
+#         for j in range(2):
+#             if j == 0:
+#                 d[i][j] = d[i-1][0] + d[i-1][1]
+#             if j == 1:
+#                 d[i][j] = d[i-1][0]
+#
+#     return sum(d[n])
+
+# 1차원 배열로 풀기
+
+
 def solve(n):
-    d = []
-    for i in range(n+1):
-        d.append([0] * 2)
-    # d[i][0] = d[i-1][0] + d[i-1][1]
-    # d[i][1] = d[i-1][0]
-    # 초기값 설정
-    d[1][0] = 0
-    d[1][1] = 1
-
-    for i in range(2, n+1):
-        for j in range(2):
-            if j == 0:
-                d[i][j] = d[i-1][0] + d[i-1][1]
-            if j == 1:
-                d[i][j] = d[i-1][0]
-
-    return sum(d[n])
+    d = [0] * (n + 1)
+    d[1] = 1
+    for i in range(2, n + 1):
+        d[i] = d[i-1] + d[i-2]
+    return d[n]
 
 
 def main():
