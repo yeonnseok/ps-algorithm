ecc69faa127e67332884afbe3ebbe2f65291ec03
choiyeonseok <smr603@snu.ac.kr>
2019-09-06 19:17:57 +0900

    DP - 스티커 (renew)

--- 2019 baekjoon/DynamicProgramming/9465_sticker.py
+++ 2019 baekjoon/DynamicProgramming/9465_sticker.py
@@ -3,9 +3,9 @@ def solve(n, src):
     for _ in range(n + 1):
         d.append([0] * 3)
 
-    d[1][1] = d[2][1] = 0
-    d[1][2] = src[0][1]
-    d[2][2] = src[1][1]
+    # 초기값 설정
+    d[1][1] = src[0][0]
+    d[1][2] = src[1][0]
 
     for i in range(1, n + 1):
         d[i][0] = max(d[i - 1][0], d[i - 1][1], d[i - 1][2])
