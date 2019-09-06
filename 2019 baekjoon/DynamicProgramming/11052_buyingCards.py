dac0c76713cbcdbf9cd080635fe7eec276bd3574
choiyeonseok <smr603@snu.ac.kr>
2019-09-06 11:19:35 +0900

    DP - 카드구매하기 Bottom-up (renew)

--- 2019 baekjoon/DynamicProgramming/11052_buyingCards.py
+++ 2019 baekjoon/DynamicProgramming/11052_buyingCards.py
@@ -4,12 +4,7 @@
 
 def solve(src, n):
     d = [0] * (n + 1)
-    if n <= 1:
-        return src[0]
-
-    if d[n]:
-        return d[n]
-
+    d[1] = src[0]
     for i in range(1, n+1):
         for j in range(1, i+1):
             d[i] = max(d[i], d[i-j] + src[j])
