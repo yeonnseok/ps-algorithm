3d7032505671cc3a90e037e9e90dbf949546fe3e
choiyeonseok <smr603@snu.ac.kr>
2019-09-06 14:06:16 +0900

    DP - 1,2,3더하기5 Bottom-up (renew)

--- 2019 baekjoon/DynamicProgramming/15990_1,2,3adding5.py
+++ 2019 baekjoon/DynamicProgramming/15990_1,2,3adding5.py
@@ -1,25 +1,53 @@
+# def solve(n):
+#     limit = 100000
+#     mod = 1000000009
+#     d = []
+#     for _ in range(limit + 1):
+#         d.append([0] * 4)
+#
+#     for i in range(1, limit + 1):
+#         if i - 1 >= 0:
+#             d[i][1] = d[i-1][2] + d[i-1][3]
+#             if i == 1:
+#                 d[i][1] = 1
+#
+#         if i - 2 >= 0:
+#             d[i][2] = d[i-2][1] + d[i-2][3]
+#             if i == 2:
+#                 d[i][2] = 1
+#
+#         if i - 3 >= 0:
+#             d[i][3] = d[i-3][1] + d[i-3][2]
+#             if i == 3:
+#                 d[i][3] = 1
+#
+#         d[i][1] %= mod
+#         d[i][2] %= mod
+#         d[i][3] %= mod
+#
+#     return (d[n][1] + d[n][2] + d[n][3]) % mod
+
+
 def solve(n):
     limit = 100000
     mod = 1000000009
     d = []
     for _ in range(limit + 1):
         d.append([0] * 4)
+    d[0][1] = d[0][2] = d[0][3] = 1
+    d[1][1] = 1
+    d[2][1] = d[2][2] = 1
+    d[3][1] = d[3][2] = d[3][3] = 1
 
     for i in range(1, limit + 1):
-        if i - 1 >= 0:
+        if i - 1 > 0:
             d[i][1] = d[i-1][2] + d[i-1][3]
-            if i == 1:
-                d[i][1] = 1
 
-        if i - 2 >= 0:
+        if i - 2 > 0:
             d[i][2] = d[i-2][1] + d[i-2][3]
-            if i == 2:
-                d[i][2] = 1
 
-        if i - 3 >= 0:
+        if i - 3 > 0:
             d[i][3] = d[i-3][1] + d[i-3][2]
-            if i == 3:
-                d[i][3] = 1
 
         d[i][1] %= mod
         d[i][2] %= mod
