0687449456cd56aacbe9ba3cc75db4b4d3149660
choiyeonseok <smr603@snu.ac.kr>
2019-09-06 17:21:51 +0900

    DP - 제곱수 합 Bottom-up (renew)

--- 2019 baekjoon/DynamicProgramming/1699_squareAdd.py
+++ 2019 baekjoon/DynamicProgramming/1699_squareAdd.py
@@ -1,16 +1,30 @@
-def solve(n):
-    d = []
-    count = 0
-    for i in range(1, 1000):
-        if i ** 2 > n:
-            break
-        d.append(i**2)
+# def solve(n):
+#     d = []
+#     count = 0
+#     for i in range(1, 1000):
+#         if i ** 2 > n:
+#             break
+#         d.append(i**2)
+#
+#     for num in reversed(d):
+#         while num <= n:
+#             n -= num
+#             count += 1
+#     return count
 
-    for num in reversed(d):
-        while num <= n:
-            n -= num
-            count += 1
-    return count
+
+def solve(n):
+    d = [0] * (n + 1)
+
+    for i in range(1, n + 1):
+        # 1^2 을 i개 사용한것이 최대 갯수
+        d[i] = i
+        j = 1
+        while j*j < i:
+            if d[i] > d[i - j*j] + 1:
+                d[i] = d[i - j*j] + 1
+            j += 1
+    return d[n]
 
 
 def main():
