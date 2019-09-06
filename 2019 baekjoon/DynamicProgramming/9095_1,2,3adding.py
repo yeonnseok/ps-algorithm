766c007484863eae41f638ae6261c61ddeed06da
choiyeonseok <smr603@snu.ac.kr>
2019-09-06 11:07:16 +0900

    DP - 1,2,3더하기 Bottom-up (renew)

--- 2019 baekjoon/DynamicProgramming/9095_1,2,3adding.py
+++ 2019 baekjoon/DynamicProgramming/9095_1,2,3adding.py
@@ -1,13 +1,23 @@
+# def solve(n):
+#     d = [0] * (n + 1)
+#     if n <= 1:
+#         return 1
+#     if n == 2:
+#         return 2
+#     if d[n]:
+#         return d[n]
+#
+#     d[n] = solve(n-1) + solve(n-2) + solve(n-3)
+#     return d[n]
+
+
 def solve(n):
     d = [0] * (n + 1)
-    if n <= 1:
-        return 1
-    if n == 2:
-        return 2
-    if d[n]:
-        return d[n]
-
-    d[n] = solve(n-1) + solve(n-2) + solve(n-3)
+    d[0] = 1
+    d[1] = 1
+    d[2] = 2
+    for i in range(2, n + 1):
+        d[i] = d[i-1] + d[i-2] + d[i-3]
     return d[n]
 
 
