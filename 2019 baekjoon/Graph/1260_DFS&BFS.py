f746aaa0531bbdce192ca27c31bdf8e606eec762
choiyeonseok <smr603@snu.ac.kr>
2019-09-07 22:22:01 +0900

    그래프 - dfs, bfs

---
+++ 2019 baekjoon/Graph/1260_DFS&BFS.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+n, m, start = map(int, input().split())
+a = [[] for _ in range(n + 1)]
+check = [0] * (n + 1)
+
+# 인접 리스트 구현
+for i in range(m):
+    u, v = map(int, input().split())
+    a[u].append(v)
+    a[v].append(u)
+
+# 인접 리스트 정렬
+for i in range(n):
+    a[i].sort()
+
+
+def dfs(x):
+    global check
+    check[x] = True
+    print(x, end=' ')
+    for y in a[x]:
+        if check[y] == 0:
+            dfs(y)
+
+
+def bfs(start):
+    ch = [0] * (n + 1)
+    q = deque()
+    q.append(start)
+    ch[start] = True
+
+    while q:
+        x = q.popleft()
+        print(x, end=" ")
+        for y in a[x]:
+            if ch[y] == 0:
+                ch[y] = True
+                q.append(y)
+
+
+dfs(start)
+print()
+bfs(start)
