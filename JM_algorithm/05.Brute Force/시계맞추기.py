3a366749eb058dc0465cfcf214d4e5f4a62ce7f3
choiyeonseok <smr603@snu.ac.kr>
2019-09-28 14:43:26 +0900

    시간맞추기

---
+++ JM_algorithm/05.Brute Force/시계맞추기.py
@@ -0,0 +1,35 @@
+clocks = [12, 6, 6, 6, 6, 6, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12]
+con = [[0, 1, 2], [3, 7, 9, 11], [4, 10, 14, 15], [0, 4, 5, 6, 7], [6, 7, 8, 10, 12], [0, 2, 14, 15], [3, 14, 15], [4, 5, 7, 14, 15], [1, 2, 3, 4, 5], [3, 4, 5, 9, 13]]
+
+
+def areAligned(clocks):
+    for i in range(len(clocks)):
+        if clocks[i] != 12:
+            return False
+    return True
+
+
+def push(clocks, switch):
+    for i in range(len(clocks)):
+        if i in con[switch]:
+            clocks[i] += 3
+            if clocks[i] == 15:
+                clocks[i] = 3
+
+
+def solve(switch, clocks):
+    INF = 10000000000
+    if switch == 10:
+        return areAligned(clocks) if 0 else INF
+    ret = INF
+    for i in range(4):
+        ret = min(ret, i + solve(switch + 1, clocks))
+        push(clocks, switch)
+    # push 가 네번 호출 되었으니 clocks는 원래와 같은 함수가 되므로 따로 복구시켜줄 필요가없다.
+    return ret
+
+
+print(solve(0, clocks))
+
+
+
---
+++ JM_algorithm/05.Brute Force/여행하는 외판원.py
@@ -0,0 +1,27 @@
+n = 3
+a = [i for i in range(n)]
+d = [[0,2,3],[2,0,6],[3,6,0]]
+visited = [False] * n
+
+
+def tsp(path, visited, cur):
+
+    if len(visited) == n:
+        if path:
+            return cur + d[path[0]][path[-1]]
+
+    ret = 10000000000000
+    for i in range(n):
+        if visited[i]:
+            continue
+        path.append(i)
+        visited[i] = True
+        if path:
+            here = path[-1]
+            cand = tsp(path, visited, cur + d[here][i])
+            ret = min(ret, cand)
+        visited[i] = False
+        path.pop()
+    return ret
+
+print(tsp([], visited, 0))
