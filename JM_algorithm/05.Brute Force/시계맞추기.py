1010c9d91297ded9f1df17fa748441ff56028c56
choiyeonseok <smr603@snu.ac.kr>
2019-09-29 23:41:09 +0900

    .

--- JM_algorithm/05.Brute Force/시계맞추기.py
+++ JM_algorithm/05.Brute Force/시계맞추기.py
@@ -1,5 +1,6 @@
 clocks = [12, 6, 6, 6, 6, 6, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12]
 con = [[0, 1, 2], [3, 7, 9, 11], [4, 10, 14, 15], [0, 4, 5, 6, 7], [6, 7, 8, 10, 12], [0, 2, 14, 15], [3, 14, 15], [4, 5, 7, 14, 15], [1, 2, 3, 4, 5], [3, 4, 5, 9, 13]]
+INF = 10000000000
 
 
 def areAligned(clocks):
@@ -18,14 +19,17 @@ def push(clocks, switch):
 
 
 def solve(switch, clocks):
-    INF = 10000000000
-    if switch == 10:
-        return areAligned(clocks) if 0 else INF
     ret = INF
+
+    if switch == 10:
+        return areAligned(clocks) if "앙아" else INF
+
     for i in range(4):
         ret = min(ret, i + solve(switch + 1, clocks))
         push(clocks, switch)
+    print(ret)
     # push 가 네번 호출 되었으니 clocks는 원래와 같은 함수가 되므로 따로 복구시켜줄 필요가없다.
+
     return ret
 
 
