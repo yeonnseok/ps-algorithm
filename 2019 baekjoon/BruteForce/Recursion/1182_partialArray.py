5a1f7ffff37dc59d26bbf68eba73361ebcaf9e51
choiyeonseok <smr603@snu.ac.kr>
2019-10-14 22:00:33 +0900

    브루트포스 - (re)부분수열합

--- 2019 baekjoon/BruteForce/Recursion/1182_partialArray.py
+++ 2019 baekjoon/BruteForce/Recursion/1182_partialArray.py
@@ -1,20 +1,24 @@
-def solve(index, sum):
-    global cnt
-    if index == n and sum == m:
-        cnt += 1
-        return
-    if index == n and sum != m:
-        return
-
-    solve(index + 1, sum + a[index])
-    solve(index + 1, sum)
-
-
-n, m = map(int, input().split())
+n, s = map(int, input().split())
 a = list(map(int, input().split()))
-cnt = 0
-solve(0, 0)
-# sum == 0 이면 처음부터 cnt 가 1올라가므로 1을 빼줘야함.
-if m == 0:
-    cnt -= 1
-print(cnt)
+answer = 0
+# 재귀로 풀어보자
+
+
+def recursive_sum(index, cur):
+    global answer
+    # base_case
+    if index == n and cur == s:
+        answer += 1
+        return
+    if index == n and cur != s:
+        return
+
+    recursive_sum(index + 1, cur + a[index])
+    recursive_sum(index + 1, cur)
+
+
+recursive_sum(0, 0)
+# 맨처음 cur == 0일 때 아무것도 안 더하고 1이 추가 될수 있음.
+if s == 0:
+    answer -= 1
+print(answer)
