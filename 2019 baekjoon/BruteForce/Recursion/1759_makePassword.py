f0fb837d3742e8c5ea39a642378398ed5a8c34f4
choiyeonseok <smr603@snu.ac.kr>
2019-10-14 23:18:51 +0900

    브루트포스 - (re)암호 만들기

--- 2019 baekjoon/BruteForce/Recursion/1759_makePassword.py
+++ 2019 baekjoon/BruteForce/Recursion/1759_makePassword.py
@@ -1,42 +1,35 @@
-def check(password):
-    # 최소 자음 두개 + 최소 모음 한개
-    cnt_mo = 0
-    cnt_ja = 0
-    for i in range(len(password)):
-        if (password[i] == 'a') or (password[i] == 'i') or (password[i] == 'o') or (password[i] == 'e') or (password[i] == 'u'):
-            cnt_mo += 1
-        else:
-            cnt_ja += 1
+l, c = map(int, input().split())
+a = input().split()
+answer = []
 
-        if (cnt_mo >= 1) and (cnt_ja >= 2):
-            return True
+
+def check(password):
+    mo_cnt = 0
+    ja_cnt = 0
+    for ch in password:
+        if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+            mo_cnt += 1
+        else:
+            ja_cnt += 1
+
+    if mo_cnt >= 1 and ja_cnt >= 2:
+        return True
     return False
 
 
-def solve(L, al, password, i, ans):
-    # 정답을 찾은 경우
-    if len(password) == L:
-        if check(password) and (''.join(password) not in ans):
-            password.sort()
-            ans.append(''.join(password))
-            print(''.join(password))
+def make_password(index, cur, answer):
+    if len(cur) == l and index == c:
+        if check(cur):
+            cur.sort()
+            answer.append(''.join(cur))
 
-    # 불가능한 경우
-    if i >= len(al):
+    if index >= c:
         return
 
-    solve(L, al, password + [al[i]], i + 1, ans)
-    solve(L, al, password, i + 1, ans)
+    make_password(index + 1, cur + [a[index]], answer)
+    make_password(index + 1, cur, answer)
 
 
-def main():
-    src = list(map(int, input().split()))
-    L, C = src[0], src[1]
-    al = input().split()
-    password = []
-    i = 0  # 사용할지 말지 결정해야하는 문자의 인덱스
-    ans = []
-    solve(L, al, password, i, ans)
-
-
-main()
+make_password(0, [], answer)
+answer.sort()
+list(map(print, answer))
