1525c884a0c9ac255e1484951d56be56bc7ae121
choiyeonseok <smr603@snu.ac.kr>
2019-10-15 00:17:31 +0900

    브루트포스 - (re)부등호

--- 2019 baekjoon/BruteForce/Recursion/2529_Boodeungho.py
+++ 2019 baekjoon/BruteForce/Recursion/2529_Boodeungho.py
@@ -1,33 +1,37 @@
 n = int(input())
 a = input().split()
-ans = []
 check = [False] * 10
+ans = []
 
 
 def good(x, y, op):
-    if op == '<':
+    if op == ">":
         if x > y:
-            return False
-    if op == '>':
+            return True
+    elif op == "<":
         if x < y:
-            return False
-    return True
+            return True
+    return False
 
 
-def solve(index, num):
-    if index == n+1:
+def recursive_budeng(index, num):
+    if index == n + 1:
         ans.append(num)
         return
+
     for i in range(10):
         if check[i]:
             continue
-        if index == 0 or good(num[index-1], str(i), a[index-1]):
-            check[i] = True
-            solve(index+1, num+str(i))
-            check[i] = False
+        else:
+            if index == 0 or good(num[index - 1], str(i), a[index - 1]):
+                check[i] = True
+                recursive_budeng(index + 1, num + str(i))
+                check[i] = False
 
 
-solve(0, '')
+recursive_budeng(0, '')
 ans.sort()
 print(ans[-1])
 print(ans[0])
+
+
