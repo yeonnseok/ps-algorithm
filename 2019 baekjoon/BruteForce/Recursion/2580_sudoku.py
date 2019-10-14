822bac4056e0796b010ec63d1336aead9c12ec18
choiyeonseok <smr603@snu.ac.kr>
2019-10-15 01:03:33 +0900

    브루트포스 - (re)스도쿠

--- 2019 baekjoon/BruteForce/Recursion/2580_sudoku.py
+++ 2019 baekjoon/BruteForce/Recursion/2580_sudoku.py
@@ -1,37 +1,34 @@
-def square(x, y):
+n = 9
+a = [list(map(int, input().split())) for _ in range(n)]
+check_row = [[False] * (n + 1) for _ in range(n + 1)]
+check_col = [[False] * (n + 1) for _ in range(n + 1)]
+check_square = [[False] * (n + 1) for _ in range(n + 1)]
+
+
+def which_square(x, y):
     return (x//3)*3 + (y//3)
 
 
-def calc(z):
+def solve(z):
     if z == 81:
         for row in a:
             print(' '.join(map(str, row)))
         return True
     x = z // 9
     y = z % 9
+
     if a[x][y] != 0:
-        return calc(z+1)
+        return solve(z + 1)
     else:
-        for i in range(1, 10):
-            if check[x][i] is False and check2[y][i] is False and check3[square(x, y)][i] is False:
-                check[x][i] = check[y][i] = check3[square(x, y)][i] = True
+        for i in range(1, n + 1):
+            if not check_col[y][i] or not check_row[x][i] or not check_square[which_square(x, y)][i]:
+                check_col[y][i] = check_row[x][i] = check_square[which_square(x,y)][i] = True
                 a[x][y] = i
-                if calc(z+1):
+                if solve(z + 1):
                     return True
                 a[x][y] = 0
-                check[x][i] = check[y][i] = check3[square(x, y)][i] = False
-    return False
+                check_col[y][i] = check_row[x][i] = check_square[which_square(x, y)][i] = False
+        return False
 
 
-a = [list(map(int, input().split())) for _ in range(9)]
-check = [[False]*10 for _ in range(9)]
-check2 = [[False]*10 for _ in range(9)]
-check3 = [[False]*10 for _ in range(9)]
-for i in range(9):
-    for j in range(9):
-        if a[i][j] != 0:
-            check[i][a[i][j]] = True
-            check2[j][a[i][j]] = True
-            check3[square(i, j)][a[i][j]] = True
-calc(0)
-
+solve(0)
