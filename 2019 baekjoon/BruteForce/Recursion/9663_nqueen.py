33a5b6d6a647a29bca10d8c3c9c7f26783e3be9f
choiyeonseok <smr603@snu.ac.kr>
2019-10-15 11:50:38 +0900

    브루트포스 - (re)n-queen

--- 2019 baekjoon/BruteForce/Recursion/9663_nqueen.py
+++ 2019 baekjoon/BruteForce/Recursion/9663_nqueen.py
@@ -1,35 +1,35 @@
+n = int(input())
+check_col = [False] * n
+check_dig1 = [False] * (2*n - 1)
+check_dig2 = [False] * (2*n - 1)
+
+
 def check(row, col):
     if check_col[col]:
         return False
-    if check_dig[row+col]:
+    if check_dig1[row + col]:
         return False
-    if check_dig2[row-col+n-1]:
+    if check_dig2[row - col + n - 1]:
         return False
     return True
 
 
-def calc(row):
+def nqueen(row):
+    ans = 0
     if row == n:
         return 1
-    ans = 0
+
     for col in range(n):
         if check(row, col):
             check_col[col] = True
-            check_dig[row+col] = True
-            check_dig2[row-col+n-1] = True
-            a[row][col] = True
-            ans += calc(row+1)
-            a[row][col] = False
+            check_dig1[row + col] = True
+            check_dig2[row - col + n - 1] = True
+            ans += nqueen(row + 1)
             check_col[col] = False
-            check_dig[row+col] = False
-            check_dig2[row-col+n-1] = False
+            check_dig1[row + col] = False
+            check_dig2[row - col + n - 1] = False
+
     return ans
 
 
-n = int(input())
-a = [[False]*n for _ in range(n)]
-check_col = [False] * n
-check_dig = [False] * (2*n - 1)
-check_dig2 = [False] * (2*n - 1)
-
-print(calc(0))
+print(nqueen(0))
