818130f5b6d853344d500a5277ccef0f1fceb75b
choiyeonseok <smr603@snu.ac.kr>
2019-10-15 12:07:32 +0900

    브루트포스 - (re)부분 수열의 합

--- 2019 baekjoon/BruteForce/Recursion/14225_partialArray2.py
+++ 2019 baekjoon/BruteForce/Recursion/14225_partialArray2.py
@@ -1,19 +1,19 @@
-def solve(index, sum):
-    if index == n:  # 무조건 3개를 더하되 하나는 그 중 선택할지말지는 2차적인 문제
-        c[sum] = True
-        return
-
-    solve(index + 1, sum + a[index])
-    solve(index + 1, sum)
-
-
 n = int(input())
 a = list(map(int, input().split()))
-c = [False] * (n * 1000000 + 10)
-solve(0, 0)
-i = 1
-while True:
-    if c[i] is False:
+check = [False] * 100000
+
+
+def solve(index, cur):
+    if index == n:
+        check[sum(cur)] = True
+        return
+
+    solve(index + 1, cur + [a[index]])
+    solve(index + 1, cur)
+
+
+solve(0, [])
+for i in range(1, len(check)):
+    if not check[i]:
+        print(i)
         break
-    i += 1
-print(i)
