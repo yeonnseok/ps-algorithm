da7dc0ca89d421e7eb02223cc8f6ea6c7342774f
choiyeonseok <smr603@snu.ac.kr>
2019-10-23 13:31:27 +0900

    브루트 포스 - 나3곱2

---
+++ 2019 baekjoon/BruteForce/16936_div3mul2.py
@@ -0,0 +1,28 @@
+# 나3은 소인수분해시 3의 갯수가 줄어든다.
+# 곱2는 소인수분해시 2의 갯수가 늘어난다.
+
+n = int(input())
+a = list(map(int, input().split()))
+
+# 3의 갯수가 가장 많은 것이 가장 처음 와야한다.
+# 어차피 연산의 종류가 나누기 3과 곱하기 2뿐이기 때문에 쉽게 구할 수 있다.
+
+def three(num):
+    cnt = 0
+    while num > 1:
+        if num % 3 == 0:
+            cnt += 1
+            num /= 3
+        else:
+            break
+    return cnt
+
+
+for i in range(n):
+    a[i] = (a[i], three(a[i]))
+a.sort()
+a = sorted(a, key=lambda a: a[1], reverse=True)
+ans = []
+for i in range(n):
+    ans.append(a[i][0])
+print(ans)
