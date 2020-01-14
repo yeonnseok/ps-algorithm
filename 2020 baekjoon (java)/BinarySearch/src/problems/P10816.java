235c7831f7803cd0cc9068a7538956e06bec14c0
choiyeonseok <smr603@snu.ac.kr>
2020-01-14 19:30:58 +0900

    p10816 - 숫자카드2(lowerBound - upperBound)

--- 2020 baekjoon (java)/BinarySearch/src/problems/P10816.java
+++ 2020 baekjoon (java)/BinarySearch/src/problems/P10816.java
@@ -10,15 +10,16 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class P10815 {
+public class P10816 {
 
 	static int n, m;
 	static int[] cards;
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		System.setIn(new FileInputStream("src/input/P10815.txt"));
+		System.setIn(new FileInputStream("src/input/P10816.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		n = Integer.parseInt(br.readLine());
 		cards = new int[n];
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -31,32 +32,46 @@ public class P10815 {
 		m = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < m; i++) {
-			if (hasCard(Integer.parseInt(st.nextToken()))) {
-				bw.write(1 + " ");
-			} else {
-				bw.write(0 + " ");
-			}
+			int input = Integer.parseInt(st.nextToken());
+			int len = upperBound(input) - lowerBound(input);
+			
+			bw.write(len + " ");
+			
 		}
 		bw.flush();
 		bw.close();
 	}
 	
-	public static boolean hasCard(int target) {
+	public static int lowerBound(int target) {
 		int start = 0;
 		int end = cards.length - 1;
 		
 		int mid = 0;
 		while (start <= end) {
 			mid = (start + end) / 2;
-			if (cards[mid] == target) {
-				return true;
-			} else if(cards[mid] > target) {
-				end = mid - 1;
-			} else {
+			if (cards[mid] < target) {
 				start = mid + 1;
+			} else if(cards[mid] >= target) {
+				end = mid - 1;
 			}
 		}
-		return false;
+		return start;
+	}
+	
+	public static int upperBound(int target) {
+		int start = 0;
+		int end = cards.length - 1;
+		
+		int mid = 0;
+		while (start <= end) {
+			mid = (start + end) / 2;
+			if (cards[mid] <= target) {
+				start = mid + 1;
+			} else if(cards[mid] > target) {
+				end = mid - 1;
+			}
+		}
+		return start;
 	}
 
 }
