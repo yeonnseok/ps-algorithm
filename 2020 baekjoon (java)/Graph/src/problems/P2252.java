b0f3124409a595df7f595fb5b94bfcdc8bb48ffa
choiyeonseok <smr603@snu.ac.kr>
2020-01-14 21:03:52 +0900

    p2252 - 줄세우기

---
+++ 2020 baekjoon (java)/Graph/src/problems/P2252.java
@@ -0,0 +1,71 @@
+package problems;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class P2252 {
+
+	static int n, m;
+	static int[] indegree;
+	static Queue<Integer> q = new LinkedList<>();
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		System.setIn(new FileInputStream("src/input/P2252.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		indegree = new int[n + 1];
+		List<Integer> graph[] = new ArrayList[n + 1];
+		
+		for (int i = 1; i < n + 1; i++) {
+			graph[i] = new ArrayList<>();
+		}
+
+		for (int i = 1; i < m + 1; i ++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			
+			graph[from].add(to);
+			indegree[to] ++;
+		}
+		
+		for (int i = 1; i < n + 1; i++) {
+			if (indegree[i] == 0) {
+				bw.write(i + " ");
+				q.offer(i);
+			}
+		}
+		
+		while (!q.isEmpty()) {
+			int now = q.poll();
+			for (int i : graph[now]) {
+				indegree[i] --;
+				if (indegree[i] == 0) {
+					q.offer(i);
+					bw.write(i + " ");
+				}
+			}
+			
+			
+		}
+	
+			
+		bw.flush();
+		bw.close();
+	}
+}
